53982939965b3c274fa425d49cf1659afec72ab1
Michael Oldacre <mloldacre@live.com>
2020-11-10 18:39:09 -0500

    Updated complete, working on mobile view

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-'use strict'
+
+var slideIndex = [1, 1, 1];
+var slideId = ['lifescribe-slides', 'spaced-repetition-slides','space-battleship-slides'];
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) { slideIndex[no] = 1; }
+  if (n < 1) { slideIndex[no] = x.length; }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+  }
+  x[slideIndex[no] - 1].style.display = 'block';
+}
